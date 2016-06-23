dad75c0841a44e9473dbd3a4308daaf59b874d7d
baptistePrunot <ipodbat@gmail.com>
2016-06-23 23:17:48 +0200

    maj main

--- generateur.c
+++ generateur.c
@@ -1,16 +1,20 @@
 #include "liste.h"
 #define INDICE 11
 int main(){
+								char c = 'a';
 								listeChainee code;
-								int i;
-								Element* ch = malloc(sizeof(*ch));
-								ch->valeur = 'a';
-								ch->suivant = NULL;
+								int i; //compteur;
+
+								Element* ptr = code.debut;
 								initialiser(&code);
 								for(i = 0; i <= 10; i++) {
-																ajouter(&code, ch->valeur);
-																ch = obtenirElement(&code, 0);
-																ch->valeur++;
+																ajouter(&code, c);
 								}
-								afficherCarac(&code);
+								while(code.fin->valeur != 'z') {
+																ptr->valeur++;
+																afficherDF(&code);
+																if(ptr->valeur = 'z') {
+																								ptr = ptr->suivant;
+																}
+								}
 }
