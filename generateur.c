92e952b650f153d701e1c8b5a18c49aef427d5c3
baptistePrunot <ipodbat@gmail.com>
2016-06-25 00:56:44 +0200

    Le parcours fonctionne a moitie

--- generateur.c
+++ generateur.c
@@ -1,6 +1,7 @@
 #include "liste.h"
 #include "generateur.h"
 #define INDICE 11
+#define ITERATION 200
 int main(){
 								char c = 'a';
 								listeChainee code;
@@ -8,23 +9,26 @@ int main(){
 								initialiser(&code);
 								ajouter(&code, 'a');
 								Element* ptr = code.debut;
-								for(i = 0; i <= 100; i++) {
+								for(i = 0; i <= ITERATION; i++) {
 																afficherDF(&code);
 																printf("\n");
 																ptr->valeur++;
 																if(ptr->valeur == 'z') {
-																	afficherDF(&code);
-																	printf("\n");
-																	traiter(&code, &ptr);
+																								afficherDF(&code);
+																								printf("\n");
+																								traiter(&code, &ptr);
 																}
 								}
 }
 
 void traiter(listeChainee* liste, Element** e){
-	if((*e)->suivant == NULL)	{
-			ajouter(liste, 'a');
-			(*e)->valeur-=25;
-	}else{
-		*e = (*e)->suivant;
-	}
+								if((*e)->suivant == NULL) {
+																ajouter(liste, 'a');
+																while((*e)->precedent != NULL) {
+																								(*e)->valeur-=25;
+																								(*e)=(*e)->precedent;
+																}
+								}else{
+																*e = (*e)->suivant;
+								}
 }
